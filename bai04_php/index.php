12117868a2945c5385291e84926e957ef3941f4c
tnhuyhoang1605 <157784538+tnhuyhoang1605@users.noreply.github.com>
2024-01-26 13:56:49 +0700

    Add files via upload

---
+++ bai04_php/index.php
@@ -0,0 +1,32 @@
+<?php
+    $a = 5;
+    $b = 3;
+    $tong = $a + $b;
+    echo "Tong: $tong <br>";
+
+    $hieu = $a - $b;
+    echo "Hieu: $hieu <br>";
+
+    $tich = $a * $b;
+    echo "Tich: $tich <br>";
+
+    $thuong = $a / $b;
+    echo "Thuong: $thuong <br>";
+
+    $sodu = $a % $b;
+    echo "So du: $sodu <br>";
+
+    $ss_bang = $a == $b;
+    echo "So sanh: $ss_bang <br>";
+
+    $ss_khac = $a != $b;
+    echo "So sanh khac: $ss_khac <br>";
+
+    $ss_lon_hon = $a > $b;
+    echo "So sanh lon hon: $ss_lon_hon <br>";
+
+    $a = 5;
+    $a++; //$a = $a - 1;
+    $b = 6;
+    $a +=$b;
+?>
